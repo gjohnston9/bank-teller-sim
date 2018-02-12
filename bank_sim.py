dfb5f9d152de67a60bdece638cdd4eb17935847f
Greg <greg9381@gmail.com>
2018-02-11 21:41:23 -0500

    completing first draft of simulation

--- bank_sim.py
+++ bank_sim.py
@@ -1,49 +1,170 @@
 from scipy import stats
 
+from engine import Engine
+from engine import Event
+
+debug = False
+
+### Events specific to this simulation
+###
+### Each saves a reference to the simulation using it, so that we can access and
+### update that simulation's methods and state variables
+class Arrival(Event):
+    ### Represents the arrival of a new customer
+    def __init__(self, timestamp, sim):
+        self.timestamp = timestamp
+        self.sim = sim
+
+    def callback(self):
+        if not self.sim.is_closing(): # schedule the next arrival
+            next_arrival_delay = self.sim.get_rand_time_before_next_arrival()
+            next_arrival_time = self.sim.t + next_arrival_delay
+            self.sim.engine.schedule(Arrival(next_arrival_time, self.sim))
+
+        if (self.sim.num_waiting > 0) or (self.sim.num_idle == 0): # customer joins the line
+            self.sim.num_waiting += 1
+            self.sim.arrival_times.append(self.sim.t)
+        else: # customer is immediately served
+            self.sim.waiting_times.append(0) # no waiting for this customer
+            self.sim.num_idle -= 1 # a teller becomes busy
+            self.sim.num_busy += 1
+            transaction_time = self.sim.get_rand_customer_transaction_time() # schedule the end of the transaction
+            finished_time = self.sim.t + transaction_time
+            self.sim.engine.schedule(FinishedService(finished_time, self.sim))
+
+
+class FinishedService(Event):
+    ### Represents the completion of a customer's interaction with a teller
+    def __init__(self, timestamp, sim):
+        self.timestamp = timestamp
+        self.sim = sim
+
+    def callback(self):
+        self.sim.num_busy -= 1 # a teller becomes free
+        self.sim.num_idle += 1
+
+        if (self.sim.will_i_take_a_lunch_break()):
+            if debug: print("teller {} is taking a lunch break, starting at time {} and lasting for {} hours".format(
+                self.sim.num_lunch_breaks_taken, self.sim.t, self.sim.lunch_break_length))
+            self.sim.num_lunch_breaks_taken += 1
+            self.sim.num_idle -= 1 # the teller who just finished takes a lunch break
+            self.sim.num_lunch += 1
+            finished_time = self.sim.t + self.sim.lunch_break_length
+            self.sim.engine.schedule(FinishedLunchBreak(finished_time, self.sim))
+        else:
+            self.sim.engine.schedule(TellerBecomesFree(self.sim.t, self.sim)) # teller calls next customer
+
+
+class FinishedLunchBreak(Event):
+    ### Represents the completion of a teller's lunchbreak
+    def __init__(self, timestamp, sim):
+        self.timestamp = timestamp
+        self.sim = sim
+
+    def callback(self):
+        self.sim.num_lunch -= 1 # teller comes back from lunch
+        self.sim.num_idle += 1
+        self.sim.engine.schedule(TellerBecomesFree(self.sim.t, self.sim)) # teller calls next customer
+
+
+class TellerBecomesFree(Event):
+    ### Represents a teller who has just become idle, who will then call the next customer in line if there are any
+    def __init__(self, timestamp, sim):
+        self.timestamp = timestamp
+        self.sim = sim
+
+    def callback(self):
+        if (self.sim.num_waiting > 0): # serve the customer who is first in line
+            arrival_time = self.sim.arrival_times.pop(0)
+            waiting_time = self.sim.t - arrival_time # determine how long this customer has been waiting for
+            self.sim.waiting_times.append(waiting_time)
+            self.sim.num_waiting -= 1
+
+            self.sim.num_idle -= 1 # the teller becomes busy
+            self.sim.num_busy += 1
+            transaction_time = self.sim.get_rand_customer_transaction_time() # schedule the end of the transaction
+            finished_time = self.sim.t + transaction_time
+            self.sim.engine.schedule(FinishedService(finished_time, self.sim))
+
+
 class BankSimulation():
-	def __init__(self):
-		### parameters (should be args)
-		self.lunch_break_length = 0.75
-		self.num_tellers = 2
+    def __init__(self):
+        self.engine = Engine()
 
-		self.t = 0 # current time
+        ### parameters (should be args)
+        self.lunch_break_length = 0.75
+        self.num_tellers = 2
+
+        ### state variables
+        self.t = 0 # current time
+        self.num_waiting = 0 # number of customers in line
+        self.num_idle = self.num_tellers # all tellers start the day as idle
+        self.num_busy = 0 # number of busy tellers
+        self.num_lunch = 0 # number of tellers on lunch break
+        self.num_lunch_breaks_taken = 0
+
+        self.arrival_times = [] # used to determine waiting time for each customer
+        self.waiting_times = [] # returned after running simulation
+
+    ### Utility functions 
+
+    def get_truncated_normal(self, mean, std_dev, low, high):
+        a, b = (low - mean) / std_dev, (high - mean) / std_dev
+        return stats.truncnorm(a, b, loc=mean, scale=std_dev)
+
+    def in_rush_period(self):
+        return (2 < self.t < 4) or (8 < self.t < 10)
+
+    def in_lunchtime(self):
+        return (2 < self.t < 4)
+
+    def is_closing(self):
+        ### after 12 hours of operation, the bank stops accepting new customers in line
+        return (self.t >= 12)
+
+    ### Probability distributions (defined in their own functions to make testing easier)
+
+    def get_time_before_next_arrival_distribution(self):
+        if (self.in_rush_period()):
+            return self.get_truncated_normal(0.04, 0.04, 0, 0.08)
+        else:
+            return self.get_truncated_normal(0.08, 0.04, 0, 0.16)
+
+    def get_transaction_time_distribution(self):
+        return self.get_truncated_normal(0.2, 0.04, 0, 0.4)
+
+    def get_lunch_break_distribution(self):
+        return self.get_truncated_normal(3, 1, 2, 4)
+
+    ### Functions that use the distributions above to return an immediately useful value
+
+    def get_rand_time_before_next_arrival(self):
+        return self.get_time_before_next_arrival_distribution().rvs()
+
+    def get_rand_customer_transaction_time(self):
+        return self.get_transaction_time_distribution().rvs()
+
+    def will_i_take_a_lunch_break(self):
+        return (self.num_lunch_breaks_taken < self.num_tellers) and \
+            (self.t > self.get_lunch_break_distribution().rvs())
 
 
-	### Utility functions 
-
-	def get_truncated_normal(self, mean, std_dev, low, high):
-		a, b = (low - mean) / std_dev, (high - mean) / std_dev
-		return stats.truncnorm(a, b, loc=mean, scale=std_dev)
-
-	def in_rush_period(self):
-		return (2 < self.t < 4) or (8 < self.t < 10)
-
-	def in_lunchtime(self):
-		return (2 < self.t < 4)
-
-
-	### Probability distributions (defined in their own functions to make testing easier)
-
-	def get_time_before_next_arrival_distribution(self):
-		if (self.in_rush_period()):
-			return self.get_truncated_normal(0.04, 0.04, 0, 0.08)
-		else:
-			return self.get_truncated_normal(0.08, 0.04, 0, 0.16)
-
-	def get_transaction_time_distribution(self):
-		return self.get_truncated_normal(0.2, 0.04, 0, 0.4)
-
-	def get_lunch_break_distribution(self):
-		return self.get_truncated_normal(3, 1, 2, 4)
+    def run_simulation(self):
+        self.engine.schedule(Arrival(0, self))
+        event = self.engine.remove()
+        counter = 0
+        while (event != None) and (counter < float("inf")):
+            assert self.t <= event.timestamp
+            if debug: print("t is {:4.2f}\tevent timestamp is {:4.2f}\tevent type is {:22}\tnum_waiting: {}".format(
+                self.t, event.timestamp, event.__class__.__name__, self.num_waiting))
+            self.t = event.timestamp
+            event.callback()
+            event = self.engine.remove()
+            counter += 1
+        print("finished simulation")
+        return self.waiting_times
 
 
-	### Functions that use the distributions above to return an immediately useful value
-
-	def get_rand_time_before_next_arrival(self):
-		return get_time_before_next_arrival_distribution.rvs()
-
-	def get_rand_customer_transaction_time(self):
-		return self.get_transaction_time_distribution.rvs()
-
-	def will_i_take_a_lunch_break(self):
-		return (t > self.lunch_break_prob.rvs())
+if __name__ == "__main__":
+    sim = BankSimulation()
+    print(sim.run_simulation())
--- engine.py
+++ engine.py
@@ -1,39 +1,46 @@
 class Event:
-	def __init__(self, timestamp):
-		self.timestamp = timestamp
-		self.next_event = None
+    def __init__(self, timestamp):
+        self.timestamp = timestamp
+        self.next_event = None
+
+    def callback(self):
+        pass
 
 class Engine:
-	def __init__(self):
-		self.FEL = Event(-1)
+    def __init__(self):
+        self.FEL = None
 
-	def schedule(self, event):
-		if (self.FEL == None):
-			self.FEL = event
-			return
-		cur = self.FEL
-		while True:
-			if (cur.next_event == None) or (cur.timestamp <= event.timestamp):
-				break
-			cur = cur.next_event
-		event.next_event = cur.next_event
-		cur.next_event = event
+    def schedule(self, event):
+        if (self.FEL == None):
+            event.next_event = None
+            self.FEL = event
+            return
 
-	def remove(self):
-		if (self.FEL == None):
-			return None
-		event = self.FEL
-		self.FEL = self.FEL.next_event
-		return event
+        if (self.FEL.timestamp >= event.timestamp):
+            event.next_event = self.FEL
+            self.FEL = event
+        else:
+            cur = self.FEL
+            while (cur.next_event is not None) and (cur.next_event.timestamp < event.timestamp):
+                cur = cur.next_event
+            event.next_event = cur.next_event
+            cur.next_event = event
 
-	def __iter__(self):
-		cur = self.FEL
-		while (cur != None):
-			yield cur
-			cur = cur.next_event
+    def remove(self):
+        if (self.FEL == None):
+            return None
+        event = self.FEL
+        self.FEL = self.FEL.next_event
+        return event
 
-	def __str__(self):
-		if (self.FEL == None):
-			return "empty list"
-		s = ""
-		return "\n".join(map(str, (event.timestamp for event in self)))
+    def __iter__(self):
+        cur = self.FEL
+        while (cur != None):
+            yield cur
+            cur = cur.next_event
+
+    def __str__(self):
+        if (self.FEL == None):
+            return "empty list"
+        s = ""
+        return "\n".join(map(str, (event.timestamp for event in self)))
